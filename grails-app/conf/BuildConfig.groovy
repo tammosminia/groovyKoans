85a460297a55d69b28ca6989b285bd1258b1529f
Rob Brinkman <rob.brinkman@jdriven.com>
2014-05-19 21:23:40 +0200

    Corrected dependency configuration

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -50,6 +50,7 @@ grails.project.dependency.resolution = {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes e.g.
         // runtime 'mysql:mysql-connector-java:5.1.27'
         // runtime 'org.postgresql:postgresql:9.3-1100-jdbc41'
+        runtime "postgresql:postgresql:8.4-702.jdbc3"
     }
 
     plugins {
@@ -80,6 +81,6 @@ grails.project.dependency.resolution = {
         //compile ":coffee-asset-pipeline:1.5.0"
         //compile ":handlebars-asset-pipeline:1.3.0.1"
 
-        runtime "postgresql:postgresql:8.4-702.jdbc3"
+
     }
 }
