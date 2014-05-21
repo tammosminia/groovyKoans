eb42c1509c463634e933db4d5c94fe2e6bdeda6a
tammosminia <tammosminia@gmail.com>
2014-05-21 22:54:40 +0200

    wel postgresql

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -30,18 +30,14 @@ environments {
     production {
         dataSource {
             dbCreate = "create-drop"
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            driverClassName = "org.postgresql.Driver"
+            dialect = org.hibernate.dialect.PostgreSQLDialect
+
+            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+
+            url = "jdbc:postgresql://"+uri.host+uri.path
+            username = uri.userInfo.split(":")[0]
+            password = uri.userInfo.split(":")[1]
         }
-//        dataSource {
-//            dbCreate = "update"
-//            driverClassName = "org.postgresql.Driver"
-//            dialect = org.hibernate.dialect.PostgreSQLDialect
-//
-//            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-//
-//            url = "jdbc:postgresql://"+uri.host+uri.path
-//            username = uri.userInfo.split(":")[0]
-//            password = uri.userInfo.split(":")[1]
-//        }
     }
 }
