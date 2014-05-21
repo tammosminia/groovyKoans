eb0a1af87434214d9a796e7c21694d38f9e767b0
tammosminia <tammosminia@gmail.com>
2014-05-21 22:50:17 +0200

    overal in-memory db

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -29,15 +29,19 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            driverClassName = "org.postgresql.Driver"
-            dialect = org.hibernate.dialect.PostgreSQLDialect
-
-            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
-
-            url = "jdbc:postgresql://"+uri.host+uri.path
-            username = uri.userInfo.split(":")[0]
-            password = uri.userInfo.split(":")[1]
+            dbCreate = "create-drop"
+            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
         }
+//        dataSource {
+//            dbCreate = "update"
+//            driverClassName = "org.postgresql.Driver"
+//            dialect = org.hibernate.dialect.PostgreSQLDialect
+//
+//            uri = new URI(System.env.DATABASE_URL?:"postgres://test:test@localhost/test")
+//
+//            url = "jdbc:postgresql://"+uri.host+uri.path
+//            username = uri.userInfo.split(":")[0]
+//            password = uri.userInfo.split(":")[1]
+//        }
     }
 }
