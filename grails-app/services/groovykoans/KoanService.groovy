5625962f40f3ef893a5c1a38cad029df8cb05294
Rob Brinkman <rob.brinkman@jdriven.com>
2014-05-22 12:58:38 +0200

    Introduced secured version of the Groovy Shell

--- grails-app/services/groovykoans/KoanService.groovy
+++ grails-app/services/groovykoans/KoanService.groovy
@@ -2,15 +2,29 @@ package groovykoans
 
 import grails.transaction.Transactional
 import groovyKoans.Koan
+import org.codehaus.groovy.control.CompilerConfiguration
+import org.codehaus.groovy.control.customizers.SecureASTCustomizer
+import org.codehaus.groovy.syntax.Types
+
+
 
 @Transactional
 class KoanService {
 
+
+    final CompilerConfiguration compilerConfiguration
+
+    public KoanService() {
+          compilerConfiguration = createCompilerConfiguration()
+
+    }
+
+
     def runKoan(String koan) {
-        Binding binding = new Binding()
+
         def out = new ByteArrayOutputStream()
-        binding.setProperty("out", new PrintStream(out))
-        GroovyShell shell = new GroovyShell(binding)
+
+        GroovyShell shell = createSecuredGroovyShell(out)
         try {
             shell.evaluate(koan)
             [success: true, output: out.toString()]
@@ -25,6 +39,68 @@ class KoanService {
     Integer nextKoan(int number) {
         def koan = Koan.findByNumber(number + 1)
         koan?.number
+    }
+
+    private def createCompilerConfiguration() {
+        final SecureASTCustomizer secure = new SecureASTCustomizer()
+        secure.with {
+            closuresAllowed = true
+            methodDefinitionAllowed = false
+
+            importsWhitelist = []
+            staticImportsWhitelist = []
+            staticStarImportsWhitelist = []
+
+            tokensWhitelist = [
+                    Types.PLUS,
+                    Types.MINUS,
+                    Types.MULTIPLY,
+                    Types.DIVIDE,
+                    Types.MOD,
+                    Types.POWER,
+                    Types.PLUS_PLUS,
+                    Types.MINUS_MINUS,
+                    Types.COMPARE_EQUAL,
+                    Types.COMPARE_NOT_EQUAL,
+                    Types.COMPARE_LESS_THAN,
+                    Types.COMPARE_LESS_THAN_EQUAL,
+                    Types.COMPARE_GREATER_THAN,
+                    Types.COMPARE_GREATER_THAN_EQUAL,
+                    Types.EQUAL
+
+            ].asImmutable()
+
+
+            constantTypesClassesWhiteList = [
+                    Integer,
+                    Float,
+                    Long,
+                    Double,
+                    BigDecimal,
+                    Object,
+                    String,
+                    Boolean,
+                    boolean,
+                    int,
+                    List
+            ].asImmutable()
+
+            receiversClassesWhiteList = []
+
+//
+
+        }
+
+        def result  = new CompilerConfiguration()
+        result.addCompilationCustomizers(secure)
+
+        result
+    }
+
+    private def createSecuredGroovyShell(OutputStream out) {
+        def binding = new Binding();
+        binding.setProperty("out", new PrintStream(out))
+        new GroovyShell(binding, compilerConfiguration)
     }
 
 
